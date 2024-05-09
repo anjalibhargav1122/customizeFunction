c4c0804465573947a946f52d4b3a95256613992c
anjalibhargav1122 <anjalibhargav636790@gmail.com>
2024-05-09 15:01:05 +0530

    first commit

---
+++ customize function/if-else conditions.js
@@ -0,0 +1,30 @@
+
+function numCeck(num){
+    if (num > 0) {
+        console.log(`${num} is Positive Number.`);
+    }
+    else {
+        console.log(`${num} is Nagative Number.`);
+    }
+    
+}
+
+numCeck(-1);
+
+
+function check(number){
+
+    if (number % 2 == 0) {
+            console.log(`${number} is Even Number.`);
+        }
+        else {
+            console.log(`${number} is Odd Number.`);
+        }
+
+}
+
+check(4);
+
+
+
+
---
+++ customize function/loop.js
@@ -0,0 +1,15 @@
+
+ function table(num){
+
+for(let count = 1; count <=10; count++){
+let tab = count * num;
+console.log(`${num} X ${count} = ${tab}`);
+
+}
+
+ }
+
+table(4);
+
+
+
